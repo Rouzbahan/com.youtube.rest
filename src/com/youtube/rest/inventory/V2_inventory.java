5ebd536a9fb167fa669bd67972f8f4b20ff93d20
Rouzbahan <rouzbahan.r.t@gmail.com>
2014-11-04 18:49:28 -0500

    Middle of 5 - 2
    Changing the V2_inventory class to address better error-handling for a
    URL without a queryparam provided whilst it's a search URL

---
+++ src/com/youtube/rest/inventory/V2_inventory.java
@@ -0,0 +1,41 @@
+package com.youtube.rest.inventory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONArray;
+import com.youtube.dao.*;
+
+@Path("/v2/inventory")
+public class V2_inventory {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnBrandParts(@QueryParam("brand") String brand)
+			throws Exception {
+		String string = null;
+		JSONArray jsonArray = new JSONArray();
+
+		try {
+			if (brand == null)
+				return Response.status(400).entity("Error: Please specify the brand for this search").build();
+			
+			SchemaRESTfulMySQL dao = new SchemaRESTfulMySQL();
+			jsonArray = dao.queryReturnBrandParts(brand);
+			string = jsonArray.toString();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500)
+					.entity("Server was not able to process your request")
+					.build();
+		}
+
+		return Response.ok(string).build();
+	}
+
+}
