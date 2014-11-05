3bbe3da174ef2a626b52ccedb821ea5413129ef6
Rouzbahan <rouzbahan.r.t@gmail.com>
2014-11-04 19:43:10 -0500

    Middle of 5 - 3
    Using pathparam

--- src/com/youtube/rest/inventory/V2_inventory.java
+++ src/com/youtube/rest/inventory/V2_inventory.java
@@ -2,6 +2,7 @@ package com.youtube.rest.inventory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -22,12 +23,15 @@ public class V2_inventory {
 
 		try {
 			if (brand == null)
-				return Response.status(400).entity("Error: Please specify the brand for this search").build();
-			
+				return Response
+						.status(400)
+						.entity("Error: Please specify the brand for this search")
+						.build();
+
 			SchemaRESTfulMySQL dao = new SchemaRESTfulMySQL();
 			jsonArray = dao.queryReturnBrandParts(brand);
 			string = jsonArray.toString();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return Response.status(500)
@@ -37,5 +41,44 @@ public class V2_inventory {
 
 		return Response.ok(string).build();
 	}
+
+	@GET
+	@Path("/{brand}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnBrand(@PathParam("brand") String brand)
+			throws Exception {
+		String string = null;
+		JSONArray jsonArray = new JSONArray();
+
+		try {
+			if (brand == null)
+				return Response
+						.status(400)
+						.entity("Error: Please specify the brand for this search")
+						.build();
+
+			SchemaRESTfulMySQL dao = new SchemaRESTfulMySQL();
+			jsonArray = dao.queryReturnBrandParts(brand);
+			string = jsonArray.toString();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500)
+					.entity("Server was not able to process your request")
+					.build();
+		}
+
+		return Response.ok(string).build();
+	}
+	
+	/*@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnErrorOnBrand() throws Exception {
+		return Response
+				.status(400)
+				.entity("Error: Please specify the brand for this search")
+				.build();
+	}
+	*/
 
 }
